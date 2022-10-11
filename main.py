35e55895d941f01f5f726f536523675e730da0d9
Baxoner <baxoner5050@gmail.com>
2022-10-11 14:54:59 +0200

    2 tasks done

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 def task1():
     while True:
-        if input('[sudo] password for user: ') == 'qwerty':
+        if input('[sudo] password for user: ') == 'root':
             print('Password correct, thou shall pass to rest of the code')
             break
         else:
             print('Try again')
 
+def task2():
+    nums = str.split(input('Enter 6 numbers the space as a separator: '), ' ')
+    print(nums)
+    sum = 0
+    for i in range(6):
+        sum += int(i)
+    if sum > 30:
+        print('Sum is greater than 30')
+    
+
+task1()
+task2()
