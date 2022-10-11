ae647a96513e0f7261e8a0cd3bee2c8c8bc724a1
Baxoner <baxoner5050@gmail.com>
2022-10-11 15:01:05 +0200

    3 tasks done

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ def task1():
 
 def task2():
     nums = str.split(input('Enter 6 numbers the space as a separator: '), ' ')
-    print(nums)
     sum = 0
     for i in range(6):
-        sum += int(i)
+        sum += int(nums[i])
     if sum > 30:
         print('Sum is greater than 30')
+    else:
+        print('Sum is less than 30')
     
+def task3():
+    num = int(input('Enter a number: '))
+    if num > 0:
+        print(f'{num} is a positive number')
+    elif num < 0:
+        print(f'{num} is a negative number')
+    else:
+        print(f'{num} is zero')
 
 task1()
 task2()
+task3()
